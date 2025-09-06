ea0fb22ee21fde64d983ece5994631305b1748ed
valen <valen@duck.com>
2025-09-06 19:33:40 +0000

    cambia puerto del backend y del frontend

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
 FROM oven/bun:1
 WORKDIR /usr/src/app
 
+USER root
+RUN apt-get update && apt-get install -y git && rm -rf /var/lib/apt/lists/*
+
 USER bun
+EXPOSE 80/tcp
 EXPOSE 3000/tcp
-EXPOSE 4000/tcp
 CMD ["sleep", "infinity"]
--- backend/server.ts
+++ backend/server.ts
@@ -3,9 +3,11 @@ import { serve } from "bun";
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello, Hono with Bun!"));
+const DATABASE_URL = process.env.DATABASE_URL;
+
+app.get("/", (c) => c.text(`Hello, Hono with Bun! DATABASE_URL: ${DATABASE_URL}`));
 
 serve({
   fetch: app.fetch,
-  port: 4000,
+  port: 3000,
 });

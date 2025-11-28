b29c8ab6eb4f9e0a7830081c19d05a26d04f931b
valen <24194163+valnssh@users.noreply.github.com>
2025-11-27 23:48:54 -0300

    Update constants.ts

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -2,7 +2,7 @@
 export const API_BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://127-0-0-1.sslip.io/api/v1'
-    : 'https://ffb.valn.dev/api/v1';
+    : 'https://ffb.hra.agency/api/v1';
 
 // API Endpoints
 export const API_ENDPOINTS = {
